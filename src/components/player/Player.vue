bc308d13abfe060101656830b109d95623cc168a
shen-25 <3553642457@qq.com>
2024-02-03 00:19:36 +0800

    feat:完成视频播放器

---
+++ src/components/player/Player.vue
@@ -0,0 +1,120 @@
+<template>
+  <div id="video-player"></div>
+</template>
+<script>
+import { onMounted, watch, ref, onUnmounted } from "vue";
+import xgPlayer from "xgplayer";
+
+export default {
+  name: "Player",
+  props: {
+    // 视频地址
+    videoUrl: {
+      type: String,
+      default() {
+        return "http://192.168.2.101/%E9%98%B4%E6%99%B4%E8%A1%A5%E4%B8%81.mp4";
+      },
+    },
+    // 预览图片
+    poster: {
+      type: String,
+      default: () =>
+        "http://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg",
+    },
+    // 内联模式
+    playsinline: {
+      type: Boolean,
+      default: true,
+    },
+    width: {
+      type: String,
+      default: "100%",
+    },
+    height: {
+      type: String,
+      default: "100%",
+    },
+  },
+  emits: ["triggerEvent"],
+  setup(props, { emit }) {
+    onMounted(() => {
+      initPlayer();
+    });
+    onUnmounted(() => {
+      player.value.destroy();
+      player.value = null;
+    });
+
+    const player = ref(null);
+
+    function initPlayer() {
+      const config = {
+        id: "video-player",
+        url: props.videoUrl,
+        height: props.height,
+        width: props.width,
+        // fitVideoSize: "auto",
+        loop: true,
+
+        // fluid: true,
+        /**倍速播放 */
+        playbackRate: [0.5, 0.75, 1, 1.5, 2],
+        defaultPlaybackRate: 1,
+        playsinline: props.playsinline,
+        whitelist: [""],
+        /**画中画 */
+        pip: true,
+        pipConfig: {
+          bottom: 100,
+          right: 100,
+          width: 320,
+          height: 180,
+        },
+        /**初始化首帧 */
+        videoInit: true,
+        autoplay: true,
+
+        //关闭控制条
+
+        controls: false,
+      };
+
+      //开始实例化
+      const tPlayer = new xgPlayer(config);
+      if (tPlayer) {
+        player.value = tPlayer;
+        /* 这里注册监听 */
+        // 监听视频开始播放 播放传给父组件的是true
+        player.value.on("play", () => {
+          emit("triggerEvent", true);
+        });
+        // 监听视频已经暂停 暂停传给父组件的是true
+        player.value.on("pause", () => {
+          emit("triggerEvent", false);
+        });
+        //监听 视频退出全屏
+        player.value.on("exitFullscreen", () => {
+          window.scrollTo(0, 0);
+          console.log("已经退出全屏了");
+        });
+      }
+    }
+    watch(
+      () => props.videoUrl,
+      (newUrl) => {
+        if (!player.value) {
+          initPlayer();
+        } else {
+          player.value.src = newUrl;
+        }
+      }
+    );
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.video-player {
+  flex: auto;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <!-- 播放的视频容器 -->
+  <div class="videoPlayer">
+    <Player class="video" />
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import Player from "@/components/player/Player.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    Player,
+  },
+};
+</script>
+<style lang="scss" scoped>
+.videoPlayer {
+  position: absolute;
+  top: 44px;
+  left: 0px;
+  right: 0px;
+  bottom: 24px;
+  max-height: 100%;
+  max-width: 100%;
+  margin: 0 auto auto auto;
+  .video {
+    display: flex;
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
